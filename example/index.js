9bd75a2f4b670d8db6bfe0c3bff57ad8f388c0a0
pankaj <pankaj@ucreate.co.in>
2020-01-21 13:03:03 +0530

    add example of fcm

---
+++ example/index.js
@@ -0,0 +1,28 @@
+const { FCM } = require('../index');
+const GOOGLE_KEY = ''; // google api key
+const token = ''; // device token from google app
+const data = {
+  message: 'hello world',
+  title: 'App name',
+  notificationCode: 1,
+  data: {},
+}; // message object
+const fcm = new FCM(GOOGLE_KEY); // set the google key
+// call the fcm push method with promise
+fcm
+  .sendPromise(token, data)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// call the fcm push method with callback
+fcm.sendpush(token, data, function(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+  }
+});
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-module.exports = require('./lib/FCM');
-module.exports = require('./lib/APN');
+const FCM = require('./lib/FCM');
+const APN = require('./lib/APN');
+module.exports = {
+  FCM,
+  APN,
+};
